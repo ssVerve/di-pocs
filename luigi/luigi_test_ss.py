8fd7a4f5c93cccf12f4e86fb6c48005625534379
scott <scottstewart@Scott-Stewart-MAC.local>
2017-06-16 12:22:13 -0400

    Now uses S3 "flag" target/tasks for _SUCCESS (and other) verification and writes to S3

--- luigi/luigi_test_ss.py
+++ luigi/luigi_test_ss.py
@@ -1,21 +1,26 @@
-import luigi
 import string
 import random
+from luigi import Task,ExternalTask,LocalTarget,DateParameter 
+from luigi.contrib.s3 import S3FlagTarget
+from vrvm.myboto.s3 import S3
 
-# def rand(size=6, chars=string.ascii_uppercase + string.digits):
-#   return ''.join(random.choice(chars) for _ in range(size))
 
-class DatedExternalTask(luigi.ExternalTask):
-  date = luigi.DateParameter()
-  def output(self): return luigi.LocalTarget("pocOutput/%s/%s.tsv" % (self.__class__.__name__, self.date))
+s3 = S3()
+
+def rand(size=32, chars=string.ascii_uppercase + string.digits):
+  return ''.join(random.choice(chars) for _ in range(size))
+
+class DatedExternalTask(ExternalTask):
+  date = DateParameter()
+  def output(self): return LocalTarget("pocOutput/%s/%s.tsv" % (self.__class__.__name__, self.date))
 
 class DWDimsStageToS3(DatedExternalTask): pass
 class RTBLog(DatedExternalTask): pass
 class AdcelLog(DatedExternalTask): pass
 
-class DatedDummyTask(luigi.Task):
-  date = luigi.DateParameter()
-  def output(self): return luigi.LocalTarget("pocOutput/%s/%s.tsv" % (self.__class__.__name__, self.date))
+class DatedDummyTask(Task):
+  date = DateParameter()
+  def output(self): return LocalTarget("pocOutput/%s/%s.tsv" % (self.__class__.__name__, self.date))
   def run(self):
     with self.output().open('w') as outFile:
       for target in self.input():
@@ -23,44 +28,57 @@ class DatedDummyTask(luigi.Task):
           for line in inFile:
             outFile.write('%s-%s' % (self.__class__.__name__, line))
 
+class S3FlagDatedDummyTask(Task):
+  date = DateParameter()
+  def output(self): return S3FlagTarget('s3://verve-home/scottstewart/luigi/%s/%s/' % (self.__class__.__name__,self.date))
+  def run(self):
+    outPath = self.output().path
+    for i in range(2): 
+      s3.put(outPath + ('part-0000%s' % i), rand()) 
+    s3.put(outPath + self.output().flag, '')
+    
+class S3FlagPrefixTarget(S3FlagTarget): # only checks for prefix of flag instead that key exists for flight-configs
+  def exists(self): return any(True for _ in s3._ls(self.path + self.flag))
+
 class DWDimsLoadToRedshift(DatedDummyTask):
   def requires(self): return [DWDimsStageToS3(self.date)]
 class UpdateTargetTactic(DatedDummyTask):
   def requires(self): return [DWDimsLoadToRedshift(self.date)]
-class FlightConfigsPois(DatedDummyTask):
+class FlightConfigsPois(S3FlagDatedDummyTask):
+  def output(self): return S3FlagPrefixTarget('s3://verve-home/scottstewart/luigi/%s/' % (self.__class__.__name__), flag='updated_%s' % self.date)
   def requires(self): return [UpdateTargetTactic(self.date), DWDimsLoadToRedshift(self.date)]
 
-class RTBIngest(DatedDummyTask):
+class RTBIngest(S3FlagDatedDummyTask):
   def requires(self): return [RTBLog(self.date)]
-class AdcelRaw2Post(DatedDummyTask):
+class AdcelRaw2Post(S3FlagDatedDummyTask): # not really a flag task, doesn't currently write _SUCCESS, database check
   def requires(self): return [AdcelLog(self.date)]
-class RTBCleanAndTrim(DatedDummyTask):
+class RTBCleanAndTrim(S3FlagDatedDummyTask):
   def requires(self): return [RTBIngest(self.date)]
-class AdcelCleanAndTrim(DatedDummyTask):
+class AdcelCleanAndTrim(S3FlagDatedDummyTask):
   def requires(self): return [AdcelRaw2Post(self.date)]
-class Sessions(DatedDummyTask):
+class Sessions(S3FlagDatedDummyTask):
   def requires(self): return [RTBCleanAndTrim(self.date), AdcelCleanAndTrim(self.date)]
-class SummarizeGHs(DatedDummyTask):
+class SummarizeGHs(S3FlagDatedDummyTask):
   def requires(self): return [Sessions(self.date)]
-class Visits(DatedDummyTask):
+class Visits(S3FlagDatedDummyTask):
   def requires(self): return [SummarizeGHs(self.date), FlightConfigsPois(self.date)]
 
 
-# class DateEmitter(luigi.Task):
-#   date = luigi.DateParameter()
+# class DateEmitter(Task):
+#   date = DateParameter()
 # 
 #   def output(self):
-#     return luigi.LocalTarget("pocOutput/DateEmitter_%s.tsv" % self.date)
+#     return LocalTarget("pocOutput/DateEmitter_%s.tsv" % self.date)
 #   def run(self):
 #     with self.output().open('w') as outFile:
 #       for _ in range(random.gauss(100, 20)):
 #         outFile.write(id_generator(16))
 # 
-# class LocalDatedTask(luigi.Task):
-#   date = luigi.DateParameter()
+# class LocalDatedTask(Task):
+#   date = DateParameter()
 # 
 #   def output(self):
-#     return luigi.LocalTarget("pocOutput/LocalDatedTask_%s.tsv" % self.date)
+#     return LocalTarget("pocOutput/LocalDatedTask_%s.tsv" % self.date)
 # 
 # 
 #   def run(self):
