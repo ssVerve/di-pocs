b8cda4633d889a64dea54eff56fc7b2b18cd8c06
scott <scottstewart@ScottStewartMAC.fios-router.home>
2017-07-10 14:54:42 -0400

    adcel check

--- luigi/luigi_test_ss.py
+++ luigi/luigi_test_ss.py
@@ -3,6 +3,7 @@ import random
 from luigi import Task,ExternalTask,LocalTarget,DateParameter 
 from luigi.contrib.s3 import S3FlagTarget
 from vrvm.myboto.s3 import S3
+from vrvm import ctrl_tbl_api
 
 
 s3 = S3()
@@ -16,7 +17,7 @@ class DatedExternalTask(ExternalTask):
 
 class DWDimsStageToS3(DatedExternalTask): pass
 class RTBLog(DatedExternalTask): pass
-class AdcelLog(DatedExternalTask): pass
+# class AdcelLog(DatedExternalTask): pass
 
 class DatedDummyTask(Task):
   date = DateParameter()
@@ -37,8 +38,16 @@ class S3FlagDatedDummyTask(Task):
       s3.put(outPath + ('part-0000%s' % i), rand()) 
     s3.put(outPath + self.output().flag, '')
     
-class S3FlagPrefixTarget(S3FlagTarget): # only checks for prefix of flag instead that key exists for flight-configs
+class S3FlagPrefixTarget(S3FlagTarget):
   def exists(self): return any(True for _ in s3._ls(self.path + self.flag))
+    
+class AdcelRaw2PostTarget(S3FlagTarget): # only checks for prefix of flag instead that key exists for flight-configs
+  def __init__(self, path, date, ctrl_table_dsn='clientrds-dw-prod-control'):
+    super(S3FlagTarget, self).__init__(path, flag=None)
+    self.date = date
+    self.ctrl_table_dsn = ctrl_table_dsn
+  def exists(self): 
+    return ctrl_tbl_api.CtrlTblApi(self.ctrl_table_dsn).count_unprocessed_logs('reporting', self.date) < 1
 
 class DWDimsLoadToRedshift(DatedDummyTask):
   def requires(self): return [DWDimsStageToS3(self.date)]
@@ -51,7 +60,8 @@ class FlightConfigsPois(S3FlagDatedDummyTask):
 class RTBIngest(S3FlagDatedDummyTask):
   def requires(self): return [RTBLog(self.date)]
 class AdcelRaw2Post(S3FlagDatedDummyTask): # not really a flag task, doesn't currently write _SUCCESS, database check
-  def requires(self): return [AdcelLog(self.date)]
+  def output(self): return AdcelRaw2PostTarget('s3://verve-home/scottstewart/luigi/%s/%s/' % (self.__class__.__name__,self.date), self.date)
+#   def requires(self): return [AdcelLog(self.date)]
 class RTBCleanAndTrim(S3FlagDatedDummyTask):
   def requires(self): return [RTBIngest(self.date)]
 class AdcelCleanAndTrim(S3FlagDatedDummyTask):
@@ -63,27 +73,3 @@ class SummarizeGHs(S3FlagDatedDummyTask):
 class Visits(S3FlagDatedDummyTask):
   def requires(self): return [SummarizeGHs(self.date), FlightConfigsPois(self.date)]
 
-
-# class DateEmitter(Task):
-#   date = DateParameter()
-# 
-#   def output(self):
-#     return LocalTarget("pocOutput/DateEmitter_%s.tsv" % self.date)
-#   def run(self):
-#     with self.output().open('w') as outFile:
-#       for _ in range(random.gauss(100, 20)):
-#         outFile.write(id_generator(16))
-# 
-# class LocalDatedTask(Task):
-#   date = DateParameter()
-# 
-#   def output(self):
-#     return LocalTarget("pocOutput/LocalDatedTask_%s.tsv" % self.date)
-# 
-# 
-#   def run(self):
-#     with self.output().open('w') as outFile:
-#       for target in self.input():
-#         with target.open('r') as inFile:
-#           for line in inFile:
-#             outFile.write('%s:%s' % (str(self.date), line))
